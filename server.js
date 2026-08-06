4880a316c7986cdf5c2b474c8267aefe8cf454ad
Adrienette0107 <adrienette1005@gmail.com>
2026-08-06 22:51:47 +0530

    commit

--- server.js
+++ server.js
@@ -1,60 +1,68 @@
-const cors = require("cors");
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
 const path = require("path");
+
+const admin = require("firebase-admin");
+const { getFirestore } = require("firebase-admin/firestore");
+
+// Firebase Admin Key
+const serviceAccount = require("./serviceAccountKey.json");
+
+// Initialize Firebase
+admin.initializeApp({
+    credential: admin.cert(serviceAccount)
+});
+const db = getFirestore();
+console.log("Project ID:", serviceAccount.project_id);
+console.log("Client Email:", serviceAccount.client_email);
+// Firestore
 
 const app = express();
 const PORT = 5000;
 
-// Parse JSON
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // Serve frontend files
 app.use(express.static(__dirname));
 
-// Connect to SQLite database
-const db = new sqlite3.Database("database.db", (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log("Connected to SQLite database.");
+// Save user to Firestore
+app.post("/save-user", async (req, res) => {
+    try {
+
+        const { name, phone } = req.body;
+
+        if (!name || !phone) {
+            return res.status(400).json({
+                success: false,
+                error: "Name and phone are required"
+            });
+        }
+
+        await db.collection("users").add({
+            name,
+            phone,
+            createdAt: new Date()
+        });
+
+        console.log("User saved successfully!");
+
+        res.json({
+            success: true
+        });
+
+    } catch (err) {
+
+        console.error("FULL ERROR:");
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            error: err.message
+        });
     }
 });
 
-// Create users table if it doesn't exist
-db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        phone TEXT NOT NULL,
-        login_time DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-`);
-
-app.post("/save-user", (req, res) => {
-
-    console.log("Request received");
-    console.log(req.body);
-
-    const { name, phone } = req.body;
-
-    const sql = "INSERT INTO users (name, phone) VALUES (?, ?)";
-
-    db.run(sql, [name, phone], function(err) {
-
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ success: false });
-        }
-
-        console.log("Inserted:", this.lastID);
-
-        res.json({ success: true });
-    });
-});
-
-// Start server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
